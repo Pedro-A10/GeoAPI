c1dbfe0b11af78e5f82d44eadf106a8abd85a7f2
Pedro A. <pedroaugusto732@gmail.com>
2025-10-25 18:34:44 -0300

    Initial commit

---
+++ src/main/java/com/PedroA10/GeoAPI/GeoApiApplication.java
@@ -0,0 +1,13 @@
+package com.PedroA10.GeoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PedroA10/GeoAPI/GeoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PedroA10.GeoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
