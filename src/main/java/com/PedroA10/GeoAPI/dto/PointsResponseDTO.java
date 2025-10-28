74047ffe2e03a7b3688555e9fabcf05ac9f51d45
Pedro A. <pedroaugusto732@gmail.com>
2025-10-28 15:31:32 -0300

    Add PointsRequestDTO and PointsResponseDTO classes to DTO package. PointsRequestDTO represents request data for creating or updating a point of interest. PointsResponseDTO represents response data including point details, reviews count, and popularity.

---
+++ src/main/java/com/PedroA10/GeoAPI/dto/PointsResponseDTO.java
@@ -0,0 +1,33 @@
+package com.PedroA10.GeoAPI.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.geo.GeoJsonPolygon;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PointsResponseDTO {
+
+  private String id;
+  private String nameLocale;
+  private String description;
+  private String address;
+  private String city;
+  private String state;
+  private String country;
+  private String category;
+  private double rating;
+  private int reviewsCount;
+  private boolean active;
+
+  private GeoJsonPoint point;
+  private GeoJsonPolygon area;
+
+  private List<String> tags;
+  private double popularity;
+}
